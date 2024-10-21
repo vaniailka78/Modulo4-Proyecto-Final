ad8f19d19415debc7a622fcd84a16dcadc2a6cc5
vaniailka78 <vaniailka@gmail.com>
2024-10-20 21:51:22 -0400

    Adding proyecto-final-galeria-productos (modificaciones)

---
+++ proyecto-final-galeria-productos/src/app/admin/components/DeleteProductosDialog.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { Dialog, DialogTitle, DialogContent, DialogActions, Button } from '@mui/material';
+
+interface Props {
+    open: boolean;
+    onClose: () => void;
+    onConfirm: () => void;
+}
+
+const DeleteProductosDialog: React.FC<Props> = ({ open, onClose, onConfirm }) => {
+    const handleDelete = () => {
+        onConfirm();
+        onClose();
+    };
+
+    return (
+        <Dialog open={open} onClose={onClose}>
+            <DialogTitle color={"secondary"}><b>Confirmar Eliminación</b></DialogTitle>
+            <DialogContent>
+                ¿Estás seguro de que quieres eliminar este producto?
+            </DialogContent>
+            <DialogActions>
+                <Button color={"primary"} sx={{
+                    backgroundColor: '#4a148c',
+                    borderRadius: 10,
+                    padding: 2,
+                    margin:2,
+                    alignItems: 'left',
+                }} onClick={onClose}>Cancelar</Button>
+                <Button color={"primary"} sx={{
+                    backgroundColor: '#aa00ff',
+                    borderRadius: 10,
+                    padding: 2,
+                    margin:2,
+                    alignItems: 'left',
+                }} onClick={handleDelete}>Eliminar</Button>
+            </DialogActions>
+        </Dialog>
+    );
+};
+
+export default DeleteProductosDialog;
